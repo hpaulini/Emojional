8efe4de4810462ed5d8b7d168174331991984b2c
Helen Paulini <helen@MacBook-Pro-2.home>
2019-06-11 20:55:34 -0400

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton){
+        //when a user clicks a button, a message pops up that the user can then press 'OK' to exit from the message. Then the user can select another button
+        let alertController = UIAlertController(title: "We All Get EMOJIonal", message: "Hello", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
